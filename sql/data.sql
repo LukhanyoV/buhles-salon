3c3d6217683a7bc46513b64b99c873bb7ea73235
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-11-24 11:36:07 +0200

    data script for treatments

--- sql/data.sql
+++ sql/data.sql
@@ -1,1 +1,6 @@
 -- Add insert scripts here
+-- data for treatments
+INSERT INTO treatment (type, code, price) VALUES ('Pedicure', 'PED', 175.00); 
+INSERT INTO treatment (type, code, price) VALUES ('Manicure', 'MAN', 215.00); 
+INSERT INTO treatment (type, code, price) VALUES ('Make up', 'MAK', 185.00); 
+INSERT INTO treatment (type, code, price) VALUES ('Brows & Lashes', 'BAL', 240.00);

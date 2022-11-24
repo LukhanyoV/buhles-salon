fc7f89baba65bf9008629058841bbd466f523493
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-11-24 11:46:15 +0200

    added stylist data

--- sql/data.sql
+++ sql/data.sql
@@ -15,3 +15,10 @@ VALUES
 , ('Nonkululeko', 'Nooi', '0845267412')
 , ('Fanie', 'Johnson', '0725876479')
 , ('Andre', 'Vermeulen', '073669985');
+
+-- data for stylist
+INSERT INTO stylist (first_name, last_name, phone_number, commission_percentage) 
+VALUES 
+('Zezethu', 'Manyamalala', '0786448520', 0.15),
+('Kamva', 'Ndwanya', '0745896321', 0.17),
+('Khazimla', 'Mahomana', '0838549741', 0.18);
